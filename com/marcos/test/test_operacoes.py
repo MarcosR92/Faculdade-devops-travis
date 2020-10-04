392edffe3adb2ebc4835d264b13a51816a255de0
MarcosRoberto92 <m_roberto@live.com>
2020-10-03 22:09:31 -0300

    corrigindo nome da funcao nos testes

--- com/marcos/test/test_operacoes.py
+++ com/marcos/test/test_operacoes.py
@@ -2,10 +2,10 @@ def funcao(var):
 	return var + 5
 
 def test1_positivo():
-	assert func(10) == 15
+	assert funcao(10) == 15
 
 def test2_negativo():
-	assert func(-10) == -5
+	assert funcao(-10) == -5
 
 def test3_zero():
-	assert func(0) == 5
+	assert funcao(0) == 5
